40ad5d91013b5057d0eacd396e832c89f85418c2
[Ali Yilmaz] <[aliyilmaztr96@gmail.com]>
2023-07-20 19:22:17 +0300

    Tourism-Agency-System

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.patikaTourismAgencySystem.helper.Helper;
+import com.patikaTourismAgencySystem.view.MenuPageGUI;
+
+public class Main {
+    public static void main(String[] args) {
+        Helper.setLayout();
+        MenuPageGUI menuPageGUI = new MenuPageGUI();
+    }
+}
